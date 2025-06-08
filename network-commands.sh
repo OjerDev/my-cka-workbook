206314a724cadb245be835023d29de3afc3ef5d1
OjerDev <94605174+OjerDev@users.noreply.github.com>
2025-06-09 01:05:35 +0530

    Update network-commands.sh

--- network-commands.sh
+++ network-commands.sh
@@ -1,1 +1,4 @@
 curl -H "Host: www.example.com" https://<nodeinternalip>:<gatwaynodeport>
+
+kubectl run netshoot --rm -it --image=nicolaka/netshoot --restart=Never -- sh -c "ping -c 4 apache-svc"
+diff rent namespace - apache-svc.<namespace>.svc.cluster.local
